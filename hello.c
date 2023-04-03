51f2ad34dbbae67adedb3052afe370e8de960ae3
wael karman <wael.karman@gmail.com>
2023-04-03 09:34:15 +0200

    add info from current pointer of sched lib

--- hello.c
+++ hello.c
@@ -1,16 +1,25 @@
 #include <linux/module.h>    // included for all kernel modules
 #include <linux/kernel.h>    // included for KERN_INFO
 #include <linux/init.h>      // included for __init and __exit macros
+#include <linux/errno.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("my_name");
+MODULE_AUTHOR("Wael Karman");
 MODULE_DESCRIPTION("A Simple Hello World module");
+
+static int param1 = 1;
+static int param2 = 2;
+module_param(param1, int, S_IRUGO);
+module_param(param2, int, S_IRUGO);
 
 static int __init hello_init(void)
 {
-    printk(KERN_ALERT "Hello world!\n");
-    return 0;    // Non-zero return means that the module couldn't be loaded.
+    printk(KERN_ALERT "Hello world! %x %x , COMM: %s ,PID: %i \n",param1,param2,current->comm, current->pid);
+    return 0;    
 }
+
+
 
 static void __exit hello_cleanup(void)
 {
