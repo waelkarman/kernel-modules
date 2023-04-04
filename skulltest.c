080fa4ec70e7d268900e1a2a854d8df67d2e3a2b
wael karman <wael.karman@gmail.com>
2023-04-04 14:37:21 +0200

    add device registration

--- skulltest.c
+++ skulltest.c
@@ -5,6 +5,7 @@
 #include <linux/sched.h>
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
+#include <linux/cdev.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Wael Karman");
@@ -17,40 +18,8 @@ module_param(param2, charp, S_IRUGO); //can be used even the function:  module_p
 
 dev_t dev;
 
-static int __init skulltest_init(void)
-{
-    int res = alloc_chrdev_region(&dev,0,5, "skulltest");
-    if(res == 0){
-        printk(KERN_ALERT "DEVICE DRIVER registraion success");    
-    }else{
-        printk(KERN_ALERT "REGISTRATION FAILED");
-    }
-    printk(KERN_ALERT "Hello world thats a skulltest! %x %x , COMM: %s ,PID: %i \n",param1,param2,current->comm, current->pid);
-    return 0;    
-}
 
-static void __exit skulltest_cleanup(void)
-{
-    unregister_chrdev_region(dev, 5);
-    printk(KERN_ALERT "Cleaning up module.\n");
-}
-
-module_init(skulltest_init);
-module_exit(skulltest_cleanup);
-
-
-
-/*EXPORT A SYMBOT TO USE IT FROM APPLICATIONS AND OTHER MODULES*/
-int exp_func(int i)
-{
-    pr_info("%s:%d the value passed in is %d\n",
-            __func__, __LINE__, i);
-
-    return i;
-}
-EXPORT_SYMBOL(exp_func);
-
-
+/*DEFINE VERBS FOR FILE OPERATIONS*/
 loff_t skulltest_llseek(struct file *file, loff_t offset, int whence){
     loff_t retval = -EINVAL;
     return retval;
@@ -79,7 +48,7 @@ static int skulltest_release(struct inode *inode, struct file *file){
 }
 
 
-struct file_operations skulltest_fops = {
+const struct file_operations skulltest_fops = {
 	.owner	 = THIS_MODULE,
 	.llseek	 = skulltest_llseek,
 	.read	 = skulltest_read,
@@ -88,3 +57,54 @@ struct file_operations skulltest_fops = {
     .open	 = skulltest_open,
 	.release = skulltest_release,
 };
+
+
+struct cdev* skulltest_cdev = NULL;
+
+/*DEFINE LOADING-UNLOADING BEHAVIOUR */
+static int __init skulltest_init(void)
+{
+    int res0 = alloc_chrdev_region(&dev,0,5, "skulltest");
+    if(res0 == 0){
+        printk(KERN_ALERT "DEVICE DRIVER registraion success");    
+    }else{
+        printk(KERN_ALERT "REGISTRATION FAILED");
+    }
+
+    skulltest_cdev = cdev_alloc();
+    cdev_init(skulltest_cdev, &skulltest_fops);
+    skulltest_cdev->owner = THIS_MODULE;
+    skulltest_cdev->ops = &skulltest_fops;
+    int res1 = cdev_add(skulltest_cdev ,dev ,1);
+    if(res1 == 0){
+        printk(KERN_ALERT "DEVICE FILE registration success"); 
+    }else{
+        printk(KERN_ALERT "DEVICE FILE registraion success"); 
+    }
+    printk(KERN_ALERT "Hello world thats a skulltest! %x %x , COMM: %s ,PID: %i \n",param1,param2,current->comm, current->pid);
+    return 0;    
+}
+
+static void __exit skulltest_cleanup(void)
+{
+    unregister_chrdev_region(dev, 5);
+    cdev_del(skulltest_cdev);
+    printk(KERN_ALERT "Cleaning up module.\n");
+}
+
+module_init(skulltest_init);
+module_exit(skulltest_cleanup);
+
+
+
+/*EXPORT A SYMBOT TO USE IT FROM APPLICATIONS AND OTHER MODULES*/
+int exp_func(int i)
+{
+    pr_info("%s:%d the value passed in is %d\n",
+            __func__, __LINE__, i);
+
+    return i;
+}
+EXPORT_SYMBOL(exp_func);
+
+
