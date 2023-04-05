853cddd8b8be30929b29b6aa4a724f2b2a06f258
wael karman <wael.karman@gmail.com>
2023-04-05 09:10:14 +0200

    add dev file creation

--- skulltest.c
+++ skulltest.c
@@ -7,16 +7,16 @@
 #include <linux/kdev_t.h>
 #include <linux/cdev.h>
 
+#define MIN_NUM_DEV_REQ 1
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Wael Karman");
 MODULE_DESCRIPTION("A Simple Hello World module");
 
 static int param1 = 1;
-static char* param2 = "2";
+static char* param2 = "Pass a second argument.";
 module_param(param1, int, S_IRUGO); //last input is a permission parameter defined in linux/stat.h
 module_param(param2, charp, S_IRUGO); //can be used even the function:  module_param_array();
-
-dev_t dev;
 
 
 /*DEFINE VERBS FOR FILE OPERATIONS*/
@@ -47,7 +47,6 @@ static int skulltest_release(struct inode *inode, struct file *file){
     return 0;
 }
 
-
 const struct file_operations skulltest_fops = {
 	.owner	 = THIS_MODULE,
 	.llseek	 = skulltest_llseek,
@@ -59,12 +58,18 @@ const struct file_operations skulltest_fops = {
 };
 
 
+
+
+
+dev_t dev;
 struct cdev* skulltest_cdev = NULL;
+struct class *skulltest_class;
 
 /*DEFINE LOADING-UNLOADING BEHAVIOUR */
 static int __init skulltest_init(void)
 {
-    int res0 = alloc_chrdev_region(&dev,0,5, "skulltest");
+    int res0 = alloc_chrdev_region(&dev,0,MIN_NUM_DEV_REQ, "skulltest");
+    
     if(res0 == 0){
         printk(KERN_ALERT "DEVICE DRIVER registraion success");    
     }else{
@@ -73,21 +78,28 @@ static int __init skulltest_init(void)
 
     skulltest_cdev = cdev_alloc();
     cdev_init(skulltest_cdev, &skulltest_fops);
-    skulltest_cdev->owner = THIS_MODULE;
-    skulltest_cdev->ops = &skulltest_fops;
-    int res1 = cdev_add(skulltest_cdev ,dev ,1);
+    int res1 = cdev_add(skulltest_cdev ,dev ,MIN_NUM_DEV_REQ);
+
+
     if(res1 == 0){
         printk(KERN_ALERT "DEVICE FILE registration success"); 
     }else{
         printk(KERN_ALERT "DEVICE FILE registraion success"); 
     }
     printk(KERN_ALERT "Hello world thats a skulltest! %x %x , COMM: %s ,PID: %i \n",param1,param2,current->comm, current->pid);
+    
+    /* Create a class : appears at /sys/class */
+    skulltest_class = class_create(THIS_MODULE, "skulltest");
+    /* Create a device file in /dev */
+    device_create(skulltest_class, NULL, dev, NULL, "skulltest1");
+    
+    
     return 0;    
 }
 
 static void __exit skulltest_cleanup(void)
 {
-    unregister_chrdev_region(dev, 5);
+    unregister_chrdev_region(dev, 1);
     cdev_del(skulltest_cdev);
     printk(KERN_ALERT "Cleaning up module.\n");
 }
